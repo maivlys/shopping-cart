90dd172118f57c0828a97f3f0c6970c81d78fd4e
maivlys <122049862+maivlys@users.noreply.github.com>
2026-06-03 16:41:32 +0200

    fallback set to null as loading state is handled in Store file

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import { Navbar } from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import { lazy, Suspense } from "react";
+import "./App.module.css";
 
 const Store = lazy(() => import("./pages/Store"));
 const ProductPage = lazy(() => import("./pages/ProductPage"));
 const Favorites = lazy(() => import("./pages/Favorites"));
 const CheckOut = lazy(() => import("./pages/CheckOut"));
 
-import "./App.css";
-
 function App() {
   return (
     <>
       <Navbar />
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={null}>
         <Routes>
           <Route path="/" element={<Store />} />
           <Route path="/product/:id" element={<ProductPage />} />
